f848cec7c809ada32bb19436532bea6a1d3085b7
Pawan Dubey <git@pawandubey.com>
2022-12-28 19:59:27 -0500

    Add CLI handlers

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use clap::Parser;
+use operations::Operations;
 
 mod config;
 mod cli;
+mod operations;
 
 fn main() {
-    let _cli = cli::Cli::parse();
+    let cli = cli::Cli::parse();
+
+    match &cli.command {
+        cli::Commands::New { name } => Operations::new(name),
+        cli::Commands::Preview { port  } => Operations::preview(port.unwrap_or_default()),
+        cli::Commands::Publish { rebuild } => Operations::publish(*rebuild),
+    }
 }
---
+++ src/operations.rs
@@ -0,0 +1,15 @@
+pub struct Operations {}
+
+impl Operations {
+    pub fn new(name: &str) {
+        todo!("{}", name);
+    }
+
+    pub fn preview(port: u16) {
+        todo!("{}", &port);
+    }
+
+    pub fn publish(rebuild: bool) {
+        todo!("{}", &rebuild);
+    }
+}
